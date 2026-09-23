8e7db0fc8108b2ee31815d373a76a32eb2aa4f1c
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-09-23 20:49:49 +0200

    Initial commit

---
+++ src/main/java/pl/olafcio/iore/Iore.java
@@ -0,0 +1,10 @@
+package pl.olafcio.iore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Iore implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/pl/olafcio/iore/client/IoreClient.java
@@ -0,0 +1,10 @@
+package pl.olafcio.iore.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IoreClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
